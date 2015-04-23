add2c075be6a4e565e0d093436564f7d486575c7
MBEnoos <mbenusa@comcast.net>
2015-04-23 14:04:26 -0400

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -1,9 +1,18 @@
 var PythonShell = require('python-shell');
+var http = require('http');
+var fs = require('fs');
 PythonShell.defaultOptions = { scriptPath: '/home/pi/videoServer/camera.py' }
 PythonShell.run('/home/pi/videoServer/camera.py');
 
+http.createServer(function (req, res) {}).listen(1234, '127.0.0.1');
 
-var http = require('http');
-http.createServer(function (req, res) {
-  
-}).listen(1234, '127.0.0.1');
+stream = fs.createReadStream('home/pi/PathToFile')
+stream.on('open', function () {
+    res.writeHead(200, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+            "Accept-Ranges": "bytes",
+            "Content-Length": chunksize,
+            "Content-Type": "video/mp4"
+    });
+
+

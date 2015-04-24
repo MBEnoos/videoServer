a1e0f8f6e0dfc761a4ab9d5c1169f5be123eddb5
MBEnoos <mbenusa@comcast.net>
2015-04-24 14:00:53 -0400

    Update video.py

--- video.py
+++ video.py
@@ -1,1 +1,7 @@
+import io
+import picamera
 
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_recording(stream, format='h264', quality=23)
